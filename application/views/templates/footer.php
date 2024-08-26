18c35a1f0c0a89bdea08005e7d214daf6336ea95
Jayval254 <valariekimuge1@gmail.com>
2024-08-26 13:53:03 -0700

    views files

---
+++ application/views/templates/footer.php
@@ -0,0 +1,15 @@
+<footer class="main-footer">
+  <!-- <div class="pull-right hidden-xs">
+    <b>Developed </b>with <span class="text-danger"> ❤ </span> by Valeria
+  </div> -->
+  <strong> &copy; 2024 - <?php echo date('Y'); ?> - Event Management System
+</footer>
+
+<!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+<div class="control-sidebar-bg"></div>
+<!-- ./wrapper -->
+
+</body>
+
+</html>
