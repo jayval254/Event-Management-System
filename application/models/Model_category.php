ba908524f5dee218045a4f3d0536bd8e0cbe4cdc
Valarie <118373697+jayval254@users.noreply.github.com>
2024-08-29 15:24:31 +0300

    Update Model_category.php

--- application/models/Model_category.php
+++ application/models/Model_category.php
@@ -10,19 +10,19 @@ class Model_category extends CI_Model
 	public function getCategoryData($id = null) 
 	{
 		if($id) {
-			$sql = "SELECT * FROM vechile_category WHERE id = ?";
+			$sql = "SELECT * FROM events_category WHERE id = ?";
 			$query = $this->db->query($sql, array($id));
 			return $query->row_array();
 		}
 
-		$sql = "SELECT * FROM vechile_category";
+		$sql = "SELECT * FROM events_category";
 		$query = $this->db->query($sql);
 		return $query->result_array();
 	}
 
 	public function getActiveCategoryData() 
 	{
-		$sql = "SELECT * FROM vechile_category WHERE active = ?";
+		$sql = "SELECT * FROM events_category WHERE active = ?";
 		$query = $this->db->query($sql, array(1));
 		return $query->result_array();
 		
@@ -30,21 +30,21 @@ class Model_category extends CI_Model
 
 	public function create($data = '')
 	{
-		$create = $this->db->insert('vechile_category', $data);
+		$create = $this->db->insert('events_category', $data);
 		return ($create == true) ? true : false;
 	}
 
 	public function edit($data, $id)
 	{
 		$this->db->where('id', $id);
-		$update = $this->db->update('vechile_category', $data);
+		$update = $this->db->update('events_category', $data);
 		return ($update == true) ? true : false;	
 	}
 
 	public function delete($id)
 	{
 		$this->db->where('id', $id);
-		$delete = $this->db->delete('vechile_category');
+		$delete = $this->db->delete('events_category');
 		return ($delete == true) ? true : false;
 	}
 }
